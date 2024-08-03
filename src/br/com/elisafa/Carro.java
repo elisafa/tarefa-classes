84e0aede1c79ec1b717a9b5f85d1ced6368a572d
Elisafa <elisafajunior@hotmail.com>
2024-08-03 11:56:45 -0300

    first commit

---
+++ src/br/com/elisafa/Carro.java
@@ -0,0 +1,29 @@
+package br.com.elisafa;
+
+/**
+ * @author Elisafá Junior
+ * @version 1.0
+ *
+ */
+public class Carro {
+
+    private String marca;
+    private String modelo;
+    private int anoFabricacao;
+    private String chassi;
+
+    // metodo que liga o carro
+    public void ligar(){}
+
+    //método que acelera o carro
+    public void acelerar(){}
+
+    /**
+     *
+     * @return retorna a marca do carro
+     */
+    public String getMarca(){
+        return marca;
+    }
+
+}
